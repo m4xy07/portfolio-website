b4f73baadbfd809709c610d632b8c2e81008a818
Maximus <aman.10shaikh@gmail.com>
2024-04-07 17:26:55 +0530

    fixes

--- build/static/js/564.914c5704.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[564],{564:(e,o,s)=>{s.r(o),s.d(o,{Resume:()=>i});s(43);var c=s(579);const i=()=>(0,c.jsx)("div",{children:"Upcoming"})}}]);
-//# sourceMappingURL=564.914c5704.chunk.js.map
