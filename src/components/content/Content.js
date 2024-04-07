e87b5db216486c734df553e35cf404fae6067207
Maximus <aman.10shaikh@gmail.com>
2024-04-07 17:21:47 +0530

    fixes

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -21,11 +21,12 @@ export const Content = () => {
     return (
         <Container component="main" className={`${classes.main}`} maxWidth="sm">
             <Typography variant="h2" component="h1" gutterBottom>
-                <TextDecrypt text={`${Resume.basics.x_title} ${FirstName}`} />
+                <TextDecrypt text={`${Resume.basics.x_title} ${FirstName}👋`} />
             </Typography>
             <Typography variant="h5" component="h2" gutterBottom>
                 <TextDecrypt text={`a ${Resume.basics.job}`} />
                 <TextDecrypt text={`from ${Resume.basics.location.country}`} />
+                <TextDecrypt text={`${Resume.basics.tagline}`} />
             </Typography>
         </Container>
     );
